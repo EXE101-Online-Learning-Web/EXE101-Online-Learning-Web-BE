cb45ae753b1c6318f20d30677291094f41fbcacf
lequangtrieu <trieulqde160447@gmail.com>
2024-12-17 22:52:57 +0700

    update refac source

---
+++ OnlineLearningWebAPI/Configurations/DtoScopeConfig.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineLearningWebAPI.Data;
+using OnlineLearningWebAPI.DTOs.request.EmailRequest;
+
+namespace OnlineLearningWebAPI.Configurations
+{
+    public static class DtoScopeConfig
+    {
+        public static IServiceCollection AddDtoScopeConfig(this IServiceCollection services, WebApplicationBuilder builder)
+        {
+            services.AddDbContext<OnlineLearningDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")).EnableSensitiveDataLogging(false));
+
+            services.Configure<JwtConfig>(builder.Configuration.GetSection("JwtConfig"));
+
+            services.Configure<EmailSettings>(builder.Configuration.GetSection("EmailSettings"));
+            return services;
+        }
+    }
+}
