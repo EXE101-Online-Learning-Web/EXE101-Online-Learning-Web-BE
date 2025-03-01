01541d8d3a695e2ad9caa2ad583ef81fc316dd50
Levando10 <dolevan055@gmail.com>
2025-03-01 23:13:06 +0700

    DoLV2 update admin account

--- OnlineLearningWebAPI/DTOs/Response/TeacherResponse/AccountDTO.cs
+++ OnlineLearningWebAPI/DTOs/Response/TeacherResponse/AccountDTO.cs
@@ -6,6 +6,7 @@
         public string Email { get; set; } = null!;
         public string UserName { get; set; } = null!;
         public string? Avatar { get; set; }
+        public bool? IsBan { get; set; }
         public bool? IsVip { get; set; }
         public string Role { get; set; } = "Teacher";
     }
