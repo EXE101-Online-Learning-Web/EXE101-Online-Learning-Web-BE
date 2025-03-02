ca7c5d75354ff3f39cd9f6c99ccfc0bcd7a4d047
lequangtrieu <trieulqde160447@gmail.com>
2025-03-02 22:03:43 +0700

    update develop

--- OnlineLearningWebAPI/Service/ScheduleService.cs
+++ OnlineLearningWebAPI/Service/ScheduleService.cs
@@ -1,53 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc.Infrastructure;
-using OnlineLearningWebAPI.DTOs.request.ScheduleRequest;
+﻿using OnlineLearningWebAPI.DTOs.request.ScheduleRequest;
 using OnlineLearningWebAPI.Models;
 using OnlineLearningWebAPI.Repository.IRepository;
 using OnlineLearningWebAPI.Service.IService;
 
 namespace OnlineLearningWebAPI.Service
 {
-    public class ScheduleService : IScheduleService
-    {
-        private readonly IOpenAIService _openAIService;
-        private readonly IScheduleRepository _scheduleRepository;
+	public class ScheduleService : IScheduleService
+	{
+		private readonly IOpenAIService _openAIService;
+		private readonly IScheduleRepository _scheduleRepository;
 
-        public ScheduleService(IOpenAIService openAIService,
-            IScheduleRepository scheduleRepository)
-        {
-            _openAIService = openAIService;
-            _scheduleRepository = scheduleRepository;
-        }
+		public ScheduleService(IOpenAIService openAIService,
+			IScheduleRepository scheduleRepository)
+		{
+			_openAIService = openAIService;
+			_scheduleRepository = scheduleRepository;
+		}
 
-        public async Task<bool> CreateSchedule(CreateScheduleRequest request)
-        {
-            try
-            {
-                var answer = await _openAIService.GetAnswer(request.Question);
+		public async Task<bool> CreateSchedule(CreateScheduleRequest request)
+		{
+			try
+			{
+				var answer = await _openAIService.GetAnswer(request.Question);
 
-                Schedule newSchedule = new Schedule()
-                {
-                    ScheduleString = answer,
-                    UserID = request.UserId,
-                };
+				Schedule newSchedule = new Schedule()
+				{
+					ScheduleString = answer,
+					UserID = request.UserId,
+				};
 
-                await _scheduleRepository.CreateScheduleAsync(newSchedule);
-                return true;
-            } catch
-            {
-                return false;
-            }
-        }
+				await _scheduleRepository.CreateScheduleAsync(newSchedule);
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
+		}
 
-        public async Task<string> GetSchedule(string UserId)
-        {
-            try
-            {
-                var answer = await _scheduleRepository.GetScheduleByUserId(UserId);
-                return answer.ScheduleString;
-            } catch
-            {
-                return "";
-            }
-        }
-    }
+		public async Task<string> GetSchedule(string UserId)
+		{
+			try
+			{
+				var answer = await _scheduleRepository.GetScheduleByUserId(UserId);
+				if (answer.ScheduleString != null)
+				{
+					return answer.ScheduleString;
+				}
+				return "";
+			}
+			catch
+			{
+				return "";
+			}
+		}
+	}
 }
